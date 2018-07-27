c6045048825f212b9745d2a0a7a9765704377bb8
shahinalkabirmitul <mitul.cse@gmail.com>
2018-07-27 17:30:11 +0600

    Refactor customer module

--- vidly/routes/customers.js
+++ vidly/routes/customers.js
@@ -1,36 +1,15 @@
+const { Customer, validate } = require('../models/customer');
 const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
-const Joi = require('joi');
 
-const Customer = mongoose.model(
-  'Customer',
-  new mongoose.Schema({
-    name: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-    isGold: {
-      type: Boolean,
-      default: false,
-    },
-    phone: {
-      type: String,
-      required: true,
-      minlength: 5,
-      maxlength: 50,
-    },
-  })
-);
 router.get('/', async (req, res) => {
   const customers = await Customer.find().sort('name');
   res.send(customers);
 });
 
 router.post('/', async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
 
   let customer = new Customer({
@@ -44,7 +23,7 @@ router.post('/', async (req, res) => {
 });
 
 router.put('/:id', async (req, res) => {
-  const { error } = validateCustomer(req.body);
+  const { error } = validate(req.body);
   if (error) return res.status(400).send(error.details[0].message);
   const customer = await Customer.findByIdAndUpdate(
     req.params.id,
@@ -76,21 +55,5 @@ router.get('/:id', async (req, res) => {
     return res.status(404).send('The genre with the given ID was not found.');
   res.send(customer);
 });
-
-function validateCustomer(customer) {
-  const schema = {
-    name: Joi.string()
-      .min(5)
-      .max(50)
-      .required(),
-    phone: Joi.string()
-      .min(5)
-      .max(50)
-      .required(),
-    isGold: Joi.boolean(),
-  };
-
-  return Joi.validate(customer, schema);
-}
 
 module.exports = router;

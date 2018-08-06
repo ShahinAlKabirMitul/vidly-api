ba5eb2bf3cd348472225a336310653a721414a15
shahinalkabirmitul <mitul.cse@gmail.com>
2018-08-06 07:51:16 +0600

    Extracting the Config Logic

--- vidly/index.js
+++ vidly/index.js
@@ -1,4 +1,3 @@
-const config = require('config');
 const Joi = require('joi');
 Joi.objectId = require('joi-objectid')(Joi);
 const express = require('express');
@@ -6,13 +5,9 @@ const app = express();
 require('./startup/logging')();
 require('./startup/routes')(app);
 require('./startup/db')();
+require('./startup/config')();
 
 //throw new Error('Something failed during startup');
-
-if (!config.get('jwtPrivateKey')) {
-  console.error('FATAL ERROR : jwtPrivateKey is not define');
-  process.exit(1);
-}
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
